e7b5dcf14244218236303ac19686b66da8bca5dc
Pierre Carette <pierre.carette78@gmail.com>
2022-08-02 12:32:41 +0200

    first assignment

--- src/App.js
+++ src/App.js
@@ -19,6 +19,9 @@ export default function App() {
         <div className="bio">
         I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
         </div>
+        <div className="personnalNote">
+          Even if Farza said in the submission it is our app, for the moment all the contracts and the website was initially built by him, thanks for that in a first time.
+        </div>
 
         <button className="waveButton" onClick={wave}>
           Wave at Me
