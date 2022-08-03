a5012135b0c98035000766aa9852026ed1b93771
Pierre Carette <pierre.carette78@gmail.com>
2022-08-03 15:50:45 +0200

    ready to be deployed

---
+++ build/precache-manifest.0906b71f8a0d97d97b22a58344b3eb05.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "296c2e71790fdb9ce6fafad10f8433f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77c205439c4be52d65e3",
+    "url": "/static/css/main.db41e7c5.chunk.css"
+  },
+  {
+    "revision": "c23d5974e2945618490f",
+    "url": "/static/js/2.89a5c1db.chunk.js"
+  },
+  {
+    "revision": "77c205439c4be52d65e3",
+    "url": "/static/js/main.93dd14a7.chunk.js"
+  },
+  {
+    "revision": "f67c199efb9f2c29f389",
+    "url": "/static/js/runtime-main.f294effb.js"
+  }
+]);
